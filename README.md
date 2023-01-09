409536d124c52213b569e8d1511d3d30569f54b3
Sinthiya Islam <sinthiya.islamjuly35@gmail.com>
2023-01-09 20:25:34 +0000

    uploaded files

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Console-Finances
+
+## Description
+
+I have created a Finance Analysis using JavaScript which calculates the following:
+
+- The total number of months included in the dataset.
+
+- The net total amount of Profit/Losses over the entire period.
+
+- The average of the changes in Profit/Losses over the entire period.
+
+  - You will need to track what the total change in profits are from month to month and then find the average.
+  - (`Total/Number of months`)
+
+- The greatest increase in profits (date and amount) over the entire period.
+
+- The greatest decrease in profits (date and amount) over the entire period.
---
+++ starter/index.js
@@ -0,0 +1,178 @@
+/** @format */
+//variables list
+var finances = [
+  ['Jan-2010', 867884],
+['Feb-2010', 984655],
+['Mar-2010', 322013],
+['Apr-2010', -69417],
+['May-2010', 310503],
+['Jun-2010', 522857],
+['Jul-2010', 1033096],
+['Aug-2010', 604885],
+['Sep-2010', -216386],
+['Oct-2010', 477532],
+['Nov-2010', 893810],
+['Dec-2010', -80353],
+['Jan-2011', 779806],
+['Feb-2011', -335203],
+['Mar-2011', 697845],
+['Apr-2011', 793163],
+['May-2011', 485070],
+['Jun-2011', 584122],
+['Jul-2011', 62729],
+['Aug-2011', 668179],
+['Sep-2011', 899906],
+['Oct-2011', 834719],
+['Nov-2011', 132003],
+['Dec-2011', 309978],
+['Jan-2012', -755566],
+['Feb-2012', 1170593],
+['Mar-2012', 252788],
+['Apr-2012', 1151518],
+['May-2012', 817256],
+['Jun-2012', 570757],
+['Jul-2012', 506702],
+['Aug-2012', -1022534],
+['Sep-2012', 475062],
+['Oct-2012', 779976],
+['Nov-2012', 144175],
+['Dec-2012', 542494],
+['Jan-2013', 359333],
+['Feb-2013', 321469],
+['Mar-2013', 67780],
+['Apr-2013', 471435],
+['May-2013', 565603],
+['Jun-2013', 872480],
+['Jul-2013', 789480],
+['Aug-2013', 999942],
+['Sep-2013', -1196225],
+['Oct-2013', 268997],
+['Nov-2013', -687986],
+['Dec-2013', 1150461],
+['Jan-2014', 682458],
+['Feb-2014', 617856],
+['Mar-2014', 824098],
+['Apr-2014', 581943],
+['May-2014', 132864],
+['Jun-2014', 448062],
+['Jul-2014', 689161],
+['Aug-2014', 800701],
+['Sep-2014', 1166643],
+['Oct-2014', 947333],
+['Nov-2014', 578668],
+['Dec-2014', 988505],
+['Jan-2015', 1139715],
+['Feb-2015', 1029471],
+['Mar-2015', 687533],
+['Apr-2015', -524626],
+['May-2015', 158620],
+['Jun-2015', 87795],
+['Jul-2015', 423389],
+['Aug-2015', 840723],
+['Sep-2015', 568529],
+['Oct-2015', 332067],
+['Nov-2015', 989499],
+['Dec-2015', 778237],
+['Jan-2016', 650000],
+['Feb-2016', -1100387],
+['Mar-2016', -174946],
+['Apr-2016', 757143],
+['May-2016', 445709],
+['Jun-2016', 712961],
+['Jul-2016', -1163797],
+['Aug-2016', 569899],
+['Sep-2016', 768450],
+['Oct-2016', 102685],
+['Nov-2016', 795914],
+['Dec-2016', 60988],
+['Jan-2017', 138230],
+['Feb-2017', 671099]
+];
+
+// Financial Analysis 
+
+console.log("Financial Analysis");
+console.log("----------------------------");
+
+// two separate arrays creating for months and for the sums
+var monthsArr = [];
+var sumsArr = [];
+for (i = 0; i < finances.length; i++) {
+  for (j = 0; j < finances[i].length; j++) {
+    monthsArr.push(finances[i][0]);
+    sumsArr.push(finances[i][1]);
+  }
+}
+
+// removing the duplicates in the month array
+var months = [];
+for (var i = 0; i < monthsArr.length; i++) {
+  if (i % 2 === 0) {
+    months.push(monthsArr[i]);
+  }
+}
+
+//calculating the total number of months included in the dataset
+var numbOfMonths = months.length;
+console.log("Total Months: " + numbOfMonths);
+
+// removing the duplicates in the month array
+var sums = [];
+for (var i = 0; i < sumsArr.length; i++) {
+  if (i % 2 === 0) {
+    sums.push(sumsArr[i]);
+  }
+}
+
+//Calculating the net total amount of Profit/Losses over the entire period
+var totalAmount = 0;
+for (var i = 0; i < sums.length; i++) {
+  totalAmount = totalAmount + sums[i];
+}
+console.log("Total: $" + totalAmount);
+
+//calculating the difference between two elements is the average of the changes in Profit/Losses over the entire period
+var redem = [];
+for (var i = 0; i < sums.length - 1; i++) {
+  var difference = sums[i + 1] - sums[i];
+  redem.push(difference);
+}
+
+var total = 0;
+var averageChange = 0;
+for (var i = 0; i < redem.length; i++) {
+  total = total + redem[i];
+  averageChange = total / (numbOfMonths - 1);
+}
+//using toFixed() it is a method in order to show only 2 decimals
+console.log("Average  Change: $" + averageChange.toFixed(2));
+
+//calculation of the maximum increase in profits (date/amount) over the entire period.
+var maxIncrease = redem[0];
+for (var i = 1; i < redem.length; i++) {
+  if (redem[i] > maxIncrease) {
+    maxIncrease = redem[i];
+  }
+}
+var findTheBestMonth = redem.indexOf(maxIncrease);
+
+console.log(
+  "Greatest Increase in Profits: " +
+    months[findTheBestMonth + 1] + " $" + maxIncrease);
+
+// Calculating the greatest Decrease in Profits (date/month) over ther entire period.
+
+var maxDecrease = redem[0];
+for (var i = 1; i < redem.length; i++) {
+  if (redem[i] < maxDecrease) {
+    maxDecrease = redem[i];
+  }
+}
+var findTheDecreaseMonth = redem.indexOf(maxDecrease);
+
+console.log(
+  "Greatest Decrease in Profits: " +
+    months[findTheDecreaseMonth + 1] +
+    " $" +
+    maxDecrease
+);
